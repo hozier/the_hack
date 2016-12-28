a7e3ce49cf03373628b68a5fcc4041b8271242b0
P. William Hozier <phozier@umass.edu>
2016-12-28 02:12:43 -0500

    Restructuring fs

--- src/components/app.js
+++ src/components/app.js
@@ -3,19 +3,9 @@
 * which incorporates components provided by Material-UI.
 */
 import React, {Component} from 'react';
-import {lightGreen400, pink300} from 'material-ui/styles/colors';
-import RaisedButton from 'material-ui/RaisedButton';
-import Dialog from 'material-ui/Dialog';
-
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import Avatar from 'material-ui/Avatar';
-import Fingerprint from 'material-ui/svg-icons/action/fingerprint';
-import Hozier from "./img/csseuxui-128.jpg"
-import AppBarIcon from "./appbaricon"
-import {getStyles} from "./styles"
+import {AppBarIcon, Hozier, getStyles, RaisedButton, Dialog, FlatButton,
+   getMuiTheme, MuiThemeProvider, Avatar, Fingerprint, lightGreen400, pink300}
+   from "./lockerbox"
 
 const styles = getStyles()
 const muiTheme = getMuiTheme({
---
+++ src/components/lockerbox.js
@@ -0,0 +1,27 @@
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Avatar from 'material-ui/Avatar';
+import Fingerprint from 'material-ui/svg-icons/action/fingerprint';
+import {lightGreen400, pink300} from 'material-ui/styles/colors';
+
+
+import Hozier from "./img/csseuxui-128.jpg"
+import AppBarIcon from "./appbaricon"
+import {getStyles} from "./styles"
+
+module.exports = {
+   Hozier,
+   AppBarIcon,
+   getStyles,
+   RaisedButton,
+   Dialog,
+   FlatButton,
+   getMuiTheme,
+   MuiThemeProvider,
+   Avatar,
+   Fingerprint,
+   lightGreen400, pink300
+}
