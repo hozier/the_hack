afa31b8725f2ec46b4dd2e3bc5049b52ae1b2a3f
P. William Hozier <phozier@umass.edu>
2017-01-12 23:46:29 -0500

    Enhanced way UI updates homepage view

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -10,6 +10,38 @@ import withWidth from 'material-ui/utils/withWidth';
 import FullWidthSection from "./material-ui-internal-components/fullwidthsection.js"
 
 class Homepage extends React.Component {
+   constructor(props){
+      super(props)
+      this.state = {
+         tags: ['code', 'curiosity', 'imagination', 'discovery'],
+      }
+   }
+
+   cycleTags = () => {
+      var temp = this.state.tags
+      let el = temp.pop()
+      let index = 0
+      temp.splice(index, 0, el)
+      this.setState({
+         tags: temp
+      });
+   }
+
+   tick() {
+      this.cycleTags()
+   }
+
+   componentDidMount() {
+      this.timerID = setInterval(
+         () => this.tick(),
+         500
+      );
+   }
+
+   componentWillUnmount() {
+      clearInterval(this.timerID);
+   }
+
    render(){
       return(
          <div>
@@ -19,24 +51,26 @@ class Homepage extends React.Component {
                   backgroundColor={lightGreen400}
                   color={'#ffffff'}
                   size={300}
-               />
+                  />
                <h1>  / the_hack > </h1>
-               <p>An Anthology of Thoughts Stitched Together by #code</p>
+
+
+               <p>An Anthology of Thoughts Stitched Together by <b>#{this.state.tags[0]}</b></p>
                <RaisedButton label="Explore"
-               style={{marginBottom:40}}/>
+                  style={{marginBottom:40}}/>
             </div>
 
 
             <FullWidthSection style={styles.introbox}
-            useContent={true}
-            contentStyle={styles.content}
-            contentType="p"
-            className="home-purpose">
+               useContent={true}
+               contentStyle={styles.content}
+               contentType="p"
+               className="home-purpose">
                <Avatar
                   src={Hozier}
                   size={100}
                   style={{marginRight:20, marginBottom:-5}}
-               />
+                  />
                My attraction to design is what drives my interest of <span
                style={{color:pink300}}>user interfaces</span>, <span
                style={{color:pink300}}>software engineering</span> and <span
@@ -81,7 +115,7 @@ let styles = (() => {
          marginBottom: 13,
          letterSpacing: 0,
          color: typography.textDarkBlack,
-     }
+      }
    };
 })()
 
