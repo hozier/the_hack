c20b2342ec5af91029e880eca070f4b4a301cd41
P. William Hozier <phozier@umass.edu>
2017-01-13 04:38:26 -0500

    Vanilla design of blog template integrated.

--- src/components/lockerbox.js
+++ src/components/lockerbox.js
@@ -1,9 +1,11 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import HomePage from "./homepage"
+import MonologuesPage from "./monologues"
 
 module.exports = {
    HomePage,
+   MonologuesPage,
    getMuiTheme,
    MuiThemeProvider,
 }
