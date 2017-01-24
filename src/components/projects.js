02aab8390654bd790d3630c9f0e8ab8fe983cc4b
P. William Hozier <phozier@umass.edu>
2017-01-24 03:15:11 -0500

    Anchor tags added

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,31 +6,38 @@ import Hozier from "./img/csseuxui-128.jpg"
 import Paper from 'material-ui/Paper';
 import {lightGreen400, grey200} from 'material-ui/styles/colors';
 import Fingerprint from 'material-ui/svg-icons/action/fingerprint';
+import {Link} from 'react-router';
 
 
 class ProjectsPage extends React.Component{
 
    datastore = () => {
+      const base = 'https://github.com/hozier/'
       const array = [
          {
             cardHeaderTitle:"Play",
-            zDepth:1
+            zDepth:1,
+            url:`${base}play`
          },
          {
             cardHeaderTitle:"The Hack",
-            zDepth:2
+            zDepth:2,
+            url:`${base}the_hack`
          },
          {
             cardHeaderTitle:"Longest Repeated String",
-            zDepth:3
+            zDepth:3,
+            url:`${base}Longest_repeated_substring`
          },
          {
             cardHeaderTitle:"Operating Systems in C",
-            zDepth:4
+            zDepth:4,
+            url:`${base}labs.cmpsci377`
          },
          {
             cardHeaderTitle:"RESTful APIs",
-            zDepth:5
+            zDepth:5,
+            url:`${base}RESTful_API`
          },
       ]
       return array
@@ -53,15 +60,20 @@ class ProjectsPage extends React.Component{
 
    render(){
       var cards = this.data.map((row, i) => {
-         return  <Paper key={i} style={this.styles.papers} zDepth={row.zDepth} rounded={false}><h3 style={{fontSize:18}}>{row.cardHeaderTitle}</h3>
-            <div style={{backgroundColor: '#455a64', height: 230, paddingTop: 40}}>
-               <Avatar
-                  icon={<Fingerprint />}
-                  backgroundColor={'#455a64'}
-                  color={'#ffffff'}
-                  size={200}
-                  />
-            </div>
+         return  <Paper key={i} style={this.styles.papers} zDepth={row.zDepth} rounded={false}>
+            <h3 style={{fontSize:18}}>{row.cardHeaderTitle}</h3>
+
+            <a href={row.url} style={{ textDecoration: 'none' }} >
+
+               <div style={{backgroundColor: '#455a64', height: 230, paddingTop: 40}}>
+                  <Avatar
+                     icon={<Fingerprint />}
+                     backgroundColor={'#455a64'}
+                     color={'#ffffff'}
+                     size={200}
+                     />
+               </div>
+            </a>
          </Paper>
       })
 
