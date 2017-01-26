4a082b1ed24d5d5651fb76015ccff635e7bc145a
P. William Hozier <phozier@umass.edu>
2017-01-25 20:43:08 -0500

    More UI Updates

--- src/components/monologues.js
+++ src/components/monologues.js
@@ -5,26 +5,34 @@ import FlatButton from 'material-ui/FlatButton';
 import Playground from "./playground"
 import Hozier from "./img/csseuxui-128.jpg"
 import Technologies from "./img/THxTPC_600-337.png"
-
+import Avatar from 'material-ui/Avatar';
+import Build from 'material-ui/svg-icons/device/widgets';
+import Code from 'material-ui/svg-icons/maps/layers';
+import {redA200} from 'material-ui/styles/colors';
 
 class MonologuesPage extends React.Component{
 
    datastore = () => {
+      const powderedBlue = '#eff3ff'
       const array = [
          {
             cardHeaderTitle:"P. William Hozier",
             cardHeaderSubtitle:"Janurary 22, 2017",
             cardBodyTitle:"Component-Driven Development",
             cardBodySubtitle:"Thinking in ReactJS with NodeJS + Material Design",
-            imgSrc:'https://i.ytimg.com/vi/mFEoarLnnqM/maxresdefault.jpg',
+            backgroundColor:powderedBlue,
+            svgColor:redA200,
+            imgSrc:<Build/>,
             CardText:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
          },
          {
             cardHeaderTitle:"P. William Hozier",
             cardHeaderSubtitle:"Janurary 22, 2017",
-            cardBodyTitle:"Component-Driven Development",
-            cardBodySubtitle:"Thinking in ReactJS with NodeJS + Material Design",
-            imgSrc:'https://i.ytimg.com/vi/mFEoarLnnqM/maxresdefault.jpg',
+            cardBodyTitle:"Algorithms",
+            cardBodySubtitle:"A Mathemical Science",
+            backgroundColor:redA200,
+            svgColor:powderedBlue,
+            imgSrc:<Code/>,
             CardText:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
          }
       ]
@@ -42,11 +50,19 @@ class MonologuesPage extends React.Component{
                   subtitle={row.cardHeaderSubtitle}
                   avatar={Hozier}
                   />
+
                <CardMedia
+                  style={{backgroundColor: row.backgroundColor}}
                   overlay={<CardTitle title={row.cardBodyTitle} subtitle={row.cardBodySubtitle} />}
                   >
-                  <img src={row.imgSrc} />
+                  <Avatar
+                     icon={row.imgSrc}
+                     backgroundColor={row.backgroundColor}
+                     color={row.svgColor}
+                     size={400}
+                  />
                </CardMedia>
+
                <CardText>
                   {row.CardText}
                </CardText>
