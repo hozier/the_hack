cc5fe22e3a1d92a1b64fdc25cf002d796e5ddd0c
P. William Hozier <phozier@umass.edu>
2017-01-27 22:54:58 -0500

    Updated implemented footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,20 +9,15 @@ class Footer extends React.Component{
    render(){
       return (
          <div style={{textAlign:'center', fontFamily: 'Roboto', fontSize:12, backgroundColor:grey900, color:'#ffffff'}}>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br/><br/><br/>
             {'< > with ♥ using NodeJS, ReactJS and Material-UI'}
-
 
             <div style={{textAlign:'left', paddingLeft: 50, zDepth:5, fontSize:15}}>
                <br></br>
-               / the_hack > by The Product Co. <br/>
+               / the_hack > <span style={{fontFamily: 'Times', fontSize:12}}>by the Product <sup>co.</sup> est. 2015.</span> <br/>
             </div>
             <div style={{textAlign:'left', paddingLeft: 50}}>All Rights Reserved 2017.</div>
-            <br></br>
-            <br></br>
+            <br/><br/>
          </div>
       )
    }
