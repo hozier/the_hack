bc71b8290206d333ff2d1bb1a58a0a914222edee
P. William Hozier <phozier@umass.edu>
2017-02-16 02:44:33 -0500

    UI/UX: Projects (Prices) component retrofitted with bc content and svgs

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -5,6 +5,10 @@ import Academic from 'material-ui/svg-icons/action/description';
 import Collab from 'material-ui/svg-icons/action/dashboard';
 import Code from 'material-ui/svg-icons/action/code';
 import PaperEngine from "../automation/paperEngine"
+import Items from 'material-ui/svg-icons/image/style';
+import Labor from 'material-ui/svg-icons/action/build';
+import Misc from 'material-ui/svg-icons/action/settings';
+
 
 class ProjectsPage extends React.Component{
 
@@ -58,18 +62,18 @@ class ProjectsPage extends React.Component{
             <Playground
                payload={
                   <div>
-                     <h2 style={{fontWeight: '100', lineHeight: '50px' }}>Projects | Apps & Services</h2>{
+                     <h2 style={{fontWeight: '100', lineHeight: '50px' }}>Prices | Items & Accessories</h2>{
                         this.data.slice(0,0+2 +1).concat(this.data.slice(7)).map((row, i) => {
-                              return <PaperEngine key={i} collection={[row]} svg={ <Code/> }></PaperEngine>
+                              return <PaperEngine key={i} collection={[row]} svg={ <Items/> }></PaperEngine>
                         })}
 
-                     <h2 style={{fontWeight: '100', lineHeight: '50px' }}>Projects | Academic</h2>{
+                     <h2 style={{fontWeight: '100', lineHeight: '50px' }}>Prices | Labor</h2>{
                         this.data.slice(3,3+1 +1).map((row, i) => {
-                              return <PaperEngine key={i} collection={[row]} svg={ <Academic/> }></PaperEngine>
+                              return <PaperEngine key={i} collection={[row]} svg={ <Labor/> }></PaperEngine>
                         })}
-                     <h2 style={{fontWeight: '100', lineHeight: '50px' }}>Projects | Collaborative</h2>{
+                     <h2 style={{fontWeight: '100', lineHeight: '50px' }}>Prices | Misc.</h2>{
                         this.data.slice(5, 5+1 +1).map((row, i) => {
-                              return <PaperEngine key={i} collection={[row]} svg={ <Collab/> }></PaperEngine>
+                              return <PaperEngine key={i} collection={[row]} svg={ <Misc/> }></PaperEngine>
                         })}
                   </div>
                }
